fa679973be63d43f8f2c7f16d707213c02778cf8
mantie <mantiereid2@gmail.com>
2020-02-23 23:56:21 -0500

    Started adding the behind the scenes code for the rest of the program. Will finish the rest later.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Net.NetworkInformation;
+using System.Collections.Generic; 
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,5 +28,33 @@ namespace network_bandwidth_monitor
         {
             this.InitializeComponent();
         }
+
+        /// <summary>
+        ///  To store the list of network interfaces
+        /// </summary>
+        private NetworkInterface[] nicArr;
+
+
+
+      private void InitializeNetworkInterface()
+    {
+      nicArr = NetworkInterface.GetAllNetworkInterfaces(); // the list is equal to names of all the network interfaces  on the computer.
+      List<String> goodAdapters = new List<string>();  // create a new list  string called good adapters.
+
+      foreach (NetworkInterface x in nicArr)
+      {
+        if (x.SupportsMulticast && x.GetIPv4Statistics().UnicastPacketsReceived >= 1 && x.OperationalStatus.ToString() == "Up")
+        {
+
+          goodAdapters.Add(x.Name);
+
+        }
+
+      }
+
     }
+
+
+  }
+
 }
