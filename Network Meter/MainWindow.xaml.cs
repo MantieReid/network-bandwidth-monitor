8b13cc39218da63d789d825ff172668c270a4c1b
mantie <mantiereid2@gmail.com>
2020-03-11 23:50:51 -0400

    added some code to convert the csv to a excel chart.

--- Network Meter/MainWindow.xaml.cs
+++ Network Meter/MainWindow.xaml.cs
@@ -88,9 +88,55 @@ namespace Network_Meter
 
       NetworkDataGrid.VerticalContentAlignment = VerticalAlignment.Center;
      NetworkDataGrid.HorizontalAlignment = HorizontalAlignment.Center;
-     
-      
+
+
+      Microsoft.Office.Interop.Excel.Application xlexcel;
+      Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+
+      object misValue = System.Reflection.Missing.Value;
+      xlexcel = new Excel.Application();
+
+      var xlWorkBooks = xlexcel.Workbooks;
+
+      xlexcel.Visible = true;
+      Console.WriteLine(Directory.GetCurrentDirectory());
+
+      //Console.Directory.GetCurrentDirectory
+
+      var path = Path.Combine(Directory.GetCurrentDirectory(), "\\somefile.csv");
+
+      xlWorkBooks.OpenText(path, misValue, misValue, Excel.XlTextParsingType.xlDelimited,
+                           Excel.XlTextQualifier.xlTextQualifierNone, misValue, misValue,
+                           misValue, misValue, misValue, misValue, misValue, misValue, misValue,
+                           misValue, misValue, misValue, misValue);
+
+      // Set Sheet 1 as the sheet you want to work with
+      xlWorkSheet = (Excel.Worksheet)xlWorkBooks[1].Worksheets.get_Item(1);
+
+      xlWorkSheet.Shapes.AddChart(misValue, misValue, misValue, misValue, misValue).Select();
+
+      //~~> Make it a Line Chart
+      xlexcel.ActiveChart.ApplyCustomType(Excel.XlChartType.xlLineMarkers);
+
+      //~~> Set the data range
+      xlexcel.ActiveChart.SetSourceData(xlWorkSheet.Range["$A$1:$B$6"]);
+
+      //uncomment this if required
+      //xlWorkBooks[1].Close(true, misValue, misValue);
+      //xlexcel.Quit();
+
+      //releaseObject(xlWorkSheet);
+      //releaseObject(xlWorkBook);
+      //releaseObject(xlexcel);
     }
+
+
+
+
+
+
+
+  }
 
 
 
